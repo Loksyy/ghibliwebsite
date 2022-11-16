6a6a05ab0b8e5e5a7aff6632482221cc54b0f808
Loksyy <74905465+Loksyy@users.noreply.github.com>
2022-11-16 09:55:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,20 +18,27 @@ const GetAPI = async() => {
         const cardImg = document.createElement('div');
         cardImg.setAttribute('class', 'card-image');
 
+        const cardContent = document.createElement('div');
+        cardContent.setAttribute('class', 'card-content');
+
         container.appendChild(card);
         card.appendChild(cardImg);
+        card.appendChild(cardContent);
 
         const img = document.createElement('IMG');
         img.src = movie["image"]
         const h = document.createElement('h1');
+        const b = document.createElement('b');
         const p = document.createElement('p');
         
         cardImg.appendChild(img);
-        card.appendChild(h);
-        card.appendChild(p);
+        cardContent.appendChild(h);
+        cardContent.appendChild(b);
+        cardContent.appendChild(p);
 
-        h.innerHTML=movie["title"]
+        h.innerHTML=`${movie["title"]} (${movie["original_title_romanised"]})`;
         p.innerHTML=movie["description"];
+        b.innerHTML=`Release date : ${movie["release_date"]}`;
     }
 }
 
